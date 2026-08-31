095882f0d4177d011a25e68a39b57c858b3ae78b
chanakya <you@example.com>
2026-08-31 14:44:43 +0530

    Initial commit: VPC infrastructure

---
+++ 00-vpc/main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source = "git::https://github.com/Sutluru-Reddy-Chanakya/Terraform-aws-vpc.git"
+  project  = var.project
+  env      = var.env
+  is_peering_required = true 
+ 
+}
